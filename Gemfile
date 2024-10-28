db5759aecc3c4d77c4797894373411c3c6e94f5d
Omar Hawas <omarhawas94@gmail.com>
2024-10-28 16:50:04 -0400

    added devise

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem "jbuilder"
 
 gem 'webpacker', '~> 5.0'
 
+gem 'devise'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "home#index"
 
-  root "home#index"
+  devise_for :users
+
 end
